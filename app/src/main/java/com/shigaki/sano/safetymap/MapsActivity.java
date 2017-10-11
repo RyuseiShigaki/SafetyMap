bb8bc99fba63cf9fc7f9d1284206aaf20b914459
SusumuSano <sano336@icloud.com>
2017-10-11 16:00:32 +0900

    マップを拡大

--- app/src/main/java/com/shigaki/sano/safetymap/MapsActivity.java
+++ app/src/main/java/com/shigaki/sano/safetymap/MapsActivity.java
@@ -39,8 +39,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(32.876904, 130.7479490);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng kosen = new LatLng(32.876904, 130.7479490);
+        mMap.addMarker(new MarkerOptions().position(kosen).title("Marker in Kosen"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kosen, 20));
     }
 }
