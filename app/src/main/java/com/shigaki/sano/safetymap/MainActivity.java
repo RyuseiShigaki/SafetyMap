66d0684ae16678d6590c0bd8329dedbcb57f96be
Ryusei Shigaki <shigashan@me.com>
2017-10-11 15:35:48 +0900

    initial commit

---
+++ app/src/main/java/com/shigaki/sano/safetymap/MainActivity.java
@@ -0,0 +1,24 @@
+package com.shigaki.sano.safetymap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button_open_map = (Button)findViewById(R.id.button_open_map);
+        button_open_map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
